96bd4a573325ea798851142859a3051570359160
yogeshcd <gaikwadyogi2010@gmail.com>
2023-06-20 18:07:40 +0530

    Docker build added

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,7 +2,7 @@ def call(Map creds){
     withCredentials([string(credentialsId: 'aws_account_id', variable: 'aws_account_id')]) {
         // some block
         sh """
-        docker build -t ${creds.private_repo_name} .
+        docker build -t ${creds.private_repo_name}/${creds.project_name} .
         docker tag ${creds.private_repo_name}/${creds.project_name}:latest ${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com/${creds.repo_name}/${creds.project_name}:latest
         """
     }
