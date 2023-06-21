1ff9c3d469af200be61f7d397adb655c719b19b6
yogeshcd <gaikwadyogi2010@gmail.com>
2023-06-21 13:11:15 +0530

    correction

--- vars/dockerCleanUP.groovy
+++ vars/dockerCleanUP.groovy
@@ -1,6 +1,8 @@
 def call(Map creds){
-    sh """
-    docker rmi ${creds.private_repo_name}
-    docker rmi ${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com/${creds.private_repo_name}:latest
-    """
+    withCredentials([string(credentialsId: 'aws_account_id', variable: 'aws_account_id')]) {
+        sh """
+        docker rmi ${creds.private_repo_name}
+        docker rmi ${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com/${creds.private_repo_name}:latest
+        """
+    }
 }
