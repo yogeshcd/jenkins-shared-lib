393fbc91e99b95d53e71bdf2eab387bb1debdb6c
yogeshcd <gaikwadyogi2010@gmail.com>
2023-06-21 11:20:37 +0530

    Docker login changed

--- vars/dockerPushECR.groovy
+++ vars/dockerPushECR.groovy
@@ -1,12 +1,12 @@
 def call(Map creds) {
     withCredentials([string(credentialsId: 'aws_account_id', variable: 'aws_account_id')]) {
+        // aws ecr get-login-password --region ${creds.region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com
         sh """
-        aws ecr get-login-password --region ${creds.region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com
+        docker login -u AWS -p $(aws ecr get-login-password --region ${creds.region}) ${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com
+
         docker push ${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com/${creds.private_repo_name}/${creds.project_name}:latest
 
         """
     }
 }
-
-
 
