07e4e89cf798ff2cc4d166703a0e92ff7471e549
yogeshcd <gaikwadyogi2010@gmail.com>
2023-06-21 12:14:13 +0530

    docker scan

---
+++ vars/dockerImageScan.groovy
@@ -0,0 +1,8 @@
+def call(Map creds) {
+    withCredentials([string(credentialsId: 'aws_account_id', variable: 'aws_account_id')]) {
+        sh """
+        
+        trivy image ${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com/${creds.private_repo_name}/${creds.project_name}:latest
+        """
+    }
+}
