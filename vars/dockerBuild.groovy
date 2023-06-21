49a5385b7a8325f6e268a35ab17044ede6467b44
yogeshcd <gaikwadyogi2010@gmail.com>
2023-06-21 12:06:00 +0530

    correction

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -3,7 +3,7 @@ def call(Map creds){
         // some block
         sh """
         docker build -t ${creds.private_repo_name}/${creds.project_name} .
-        docker tag ${creds.private_repo_name}/${creds.project_name}:latest ${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com/${creds.repo_name}/${creds.project_name}:latest
+        docker tag ${creds.private_repo_name}/${creds.project_name}:latest ${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com/${creds.private_repo_name}/${creds.project_name}:latest
         """
     }
 }
