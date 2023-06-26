bf5469a23f65ff4907ebd8f900366a3d92c42722
yogeshcd <gaikwadyogi2010@gmail.com>
2023-06-26 18:43:12 +0530

    correction

--- vars/terraformApply.groovy
+++ vars/terraformApply.groovy
@@ -1,8 +1,11 @@
 def call(Map creds){
+    // terraform plan -var "AWS_ACCESS_KEY=${creds.access_key}" -var "AWS_SECRET_ACCESS_KEY=${creds.AWS_SECRECT_ACCESS_KEY}" --var-file=terraform.tfvars
+        
     sh """
         terraform init
         terraform validate
-        terraform plann -var "AWS_ACCESS_KEY=${creds.access_key}" -var "AWS_SECRET_ACCESS_KEY=${creds.AWS_SECRECT_ACCESS_KEY}" --var-file=terraform.tfvars
+        terraform plan --var-file=terraform.tfvars
+        
         terraform apply --var-file=terraform.tfvars --auto-approve
     """
 }
