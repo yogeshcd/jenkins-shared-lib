824d21632b6b1b65affe090c776bd86b61c1b8ff
yogeshcd <gaikwadyogi2010@gmail.com>
2023-07-04 15:59:23 +0530

    correction

--- vars/helmChartPush.groovy
+++ vars/helmChartPush.groovy
@@ -4,6 +4,13 @@ def call(Map creds) {
                     // /bin/bash HELM_CHART_VERSION=$(helm inspect chart . | grep version: | awk '{print $2}')
             //helm push helm-test-chart-0.1.0.tgz oci://aws_account_id.dkr.ecr.us-west-2.amazonaws.com/
             sh """
+            cd ${creds.helm_chart_name}
+            sed -i 's/aws_account_id/${aws_account_id}/g' values.yaml
+            sed -i 's/aws_region/${creds.region}/g' values.yaml
+            sed -i 's/aws_private_repo_name/${creds.private_repo_name}/g' values.yaml
+            cd ..
+
+
             echo $HELM_CHART_VERSION
             helm package ${creds.helm_chart_name}
             aws ecr get-login-password --region ${creds.region} | helm registry login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com
