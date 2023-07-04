9a541746d6951334a4689f5b50c482d12ab46dba
yogeshcd <gaikwadyogi2010@gmail.com>
2023-07-04 12:08:27 +0530

    correction

--- vars/helmPush.groovy
+++ vars/helmPush.groovy
@@ -1,7 +1,6 @@
 def call(Map creds) {
     withCredentials([string(credentialsId: 'aws_account_id', variable: 'aws_account_id')]) {
-        sh
-        """
+        sh """
         helm package some-package
 
         aws ecr get-login-password --region us-east-1 | helm registry login --username AWS --password-stdin someid.dkr.ecr.us-east-1.amazonaws.com
