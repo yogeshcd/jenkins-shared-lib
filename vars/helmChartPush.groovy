3843cc3c68583b80d188f2b9e5798084a5d0d239
yogeshcd <gaikwadyogi2010@gmail.com>
2023-07-04 13:29:34 +0530

    correction

--- vars/helmChartPush.groovy
+++ vars/helmChartPush.groovy
@@ -3,9 +3,10 @@ def call(Map creds) {
         // some block
                     // /bin/bash HELM_CHART_VERSION=$(helm inspect chart . | grep version: | awk '{print $2}')
             sh """
+            /bin/bash HELM_CHART_VERSION=$(helm inspect chart helm-java-app | grep version: | awk '{print $2}')
             helm package ${creds.package_name}
             aws ecr get-login-password --region ${creds.region} | helm registry login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com
-
+            
             """
 
 
