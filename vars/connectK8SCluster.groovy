c5515553b67c0ae8945290ef73071c1097b781ac
yogeshcd <gaikwadyogi2010@gmail.com>
2023-07-04 11:40:50 +0530

    cluster connect corrections

--- vars/connectK8SCluster.groovy
+++ vars/connectK8SCluster.groovy
@@ -1,8 +1,8 @@
 def call(Map creds) {
     sh """
     aws configure set aws_access_key_id "$AWS_ACCESS_KEY"
-    aws configure set aws_secret_access_key "$SECRET_KEY"
-    aws configure set region "${params.Region}"
-    aws eks --region ${params.Region} update-kubeconfig --name ${params.cluster}
+    aws configure set aws_secret_access_key "$AWS_SECRECT_ACCESS_KEY"
+    aws configure set region "${creds.region}"
+    aws eks --region ${creds.region} update-kubeconfig --name ${creds.cluster}
     """
 }
