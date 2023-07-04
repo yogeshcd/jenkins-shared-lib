1d8b612941788304cd75b033f80ed4b994d226f1
yogeshcd <gaikwadyogi2010@gmail.com>
2023-07-04 16:01:56 +0530

    correction

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -2,7 +2,7 @@ def call(Map creds) {
         withCredentials([string(credentialsId: 'aws_account_id', variable: 'aws_account_id')]) {
         // some block
             sh """
-            helm install ${creds.release_name} oci://${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com/${creds.helm_chart_name} --version $HELM_CHART_VERSION
+            helm upgrade --install ${creds.release_name} oci://${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com/${creds.helm_chart_name} --version $HELM_CHART_VERSION
             """
     }
 }
