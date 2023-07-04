82e095eeb3c04cc0fac4c8a2ee0186b28de4712c
yogeshcd <gaikwadyogi2010@gmail.com>
2023-07-04 12:07:20 +0530

    correction

--- vars/helmPush.groovy
+++ vars/helmPush.groovy
@@ -1,16 +1,15 @@
 def call(Map creds) {
-        withCredentials([string(credentialsId: 'aws_account_id', variable: 'aws_account_id')]) {
-        // some block
-            sh 
-            """
-            helm package some-package
+    withCredentials([string(credentialsId: 'aws_account_id', variable: 'aws_account_id')]) {
+        sh
+        """
+        helm package some-package
 
-            aws ecr get-login-password --region us-east-1 | helm registry login --username AWS --password-stdin someid.dkr.ecr.us-east-1.amazonaws.com
+        aws ecr get-login-password --region us-east-1 | helm registry login --username AWS --password-stdin someid.dkr.ecr.us-east-1.amazonaws.com
 
-            
-            HELM_CHART_VERSION=$(helm inspect chart . | grep version: | awk '{print $2}')
-            
-            helm push helm-test-chart-$HELM_CHART_VERSION.tgz oci://${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com/
-            """
+        
+        HELM_CHART_VERSION=$(helm inspect chart . | grep version: | awk '{print $2}')
+        
+        helm push helm-test-chart-$HELM_CHART_VERSION.tgz oci://${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com/
+        """
     }
 }
