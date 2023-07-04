90d2e98b0a27bc44e5f4ad4346bbb8e9eb73a67b
yogeshcd <gaikwadyogi2010@gmail.com>
2023-07-04 15:39:45 +0530

    correction

--- vars/helmChartPush.groovy
+++ vars/helmChartPush.groovy
@@ -8,7 +8,7 @@ def call(Map creds) {
             helm package ${creds.helm_chart_name}
             aws ecr get-login-password --region ${creds.region} | helm registry login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com
             
-            NAME="$HELM_CHART_VERSION$FILE"
+            
             helm push ${creds.helm_chart_name}-$NAME oci://${aws_account_id}.dkr.ecr.${creds.region}.amazonaws.com/
             """
 
