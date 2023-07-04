d9c0ca645d800f4c56188648e63e66f4c23ffd5a
yogeshcd <gaikwadyogi2010@gmail.com>
2023-07-04 11:20:56 +0530

    change terraform init command

--- vars/terraformApply.groovy
+++ vars/terraformApply.groovy
@@ -2,7 +2,7 @@ def call(Map creds){
     // terraform plan -var "AWS_ACCESS_KEY=${creds.access_key}" -var "AWS_SECRET_ACCESS_KEY=${creds.AWS_SECRECT_ACCESS_KEY}" --var-file=terraform.tfvars
         // terraform init -migrate-state -backend-config="access_key=$AWS_ACCESS_KEY" -backend-config="secret_key=$AWS_SECRECT_ACCESS_KEY"
     sh """
-        terraform init -reconfigure
+        terraform init -y
         terraform validate
         
         terraform plan -var "AWS_ACCESS_KEY=$AWS_ACCESS_KEY" -var "AWS_SECRET_ACCESS_KEY=$AWS_SECRECT_ACCESS_KEY" --var-file=terraform.tfvars
